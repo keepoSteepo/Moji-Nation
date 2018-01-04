26c0b77e51b3e130289fed026f3917e819a8e65f
Keepo <Keepitk.xprezz@yahoo.com>
2018-01-03 17:27:38 -0700

    Moji.- added tableView, working on 2nd screen

--- Moji Nation/ViewController.swift
+++ Moji Nation/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var miraMesa: UITableView!
+    
+    var Moji = ["🧐", "🤫", "😑", "🤖", "👁"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        miraMesa.dataSource = self
+        miraMesa.delegate = self
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Moji.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = Moji[indexPath.row]
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
